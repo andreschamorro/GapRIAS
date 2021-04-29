547f2e98a3387110754b15a9dc59fbc6468334fe
Andres Chamorro <andres.chamorro@upr.edu>
2021-04-29 17:21:22 -0400

    PlayerCollider set, trial and hit detected

--- Assets/Scripts/Player/PlayerCollider.cs
+++ Assets/Scripts/Player/PlayerCollider.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Events;
 
-
+[RequireComponent(typeof(CapsuleCollider))]
 public class PlayerCollider : MonoBehaviour
 {
+    public Transform head;
     public UnityEvent ActionOnTriggerCollision;
+
+    private CapsuleCollider capsuleCollider;
+
+    //-------------------------------------------------
+    void Awake()
+    {
+        capsuleCollider = GetComponent<CapsuleCollider>();
+    }
+
+
+    //-------------------------------------------------
+    void FixedUpdate()
+    {
+        float distanceFromFloor = Vector3.Dot(head.localPosition, Vector3.up);
+        capsuleCollider.height = Mathf.Max(capsuleCollider.radius, distanceFromFloor);
+        transform.localPosition = head.localPosition - 0.5f * distanceFromFloor * Vector3.up;
+    }
 
     public void OnTriggerEnter(Collider collider)
     {
